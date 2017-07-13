69b202b6ab779043d85617ed95513498a8b5fae7
wahyutris <wahyu.trisvianto@gmail.com>
2017-07-13 14:30:00 +0700

    realtime password validation

---
+++ BusTicketBookingSystem/BusTicketBookingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BusTicketBookingSystem.Startup))]
+namespace BusTicketBookingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
